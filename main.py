f353a26da77055619e373d58a267df22c0a81f94
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-06 20:33:35 +0200

    додала кошик-словник та наповнення його

--- main.py
+++ main.py
@@ -1,37 +1,19 @@
-"""Завдання 1
-Створіть клас "Користувач" з атрибутами "ім'я", "вік" та
-"email". Застосуйте інкапсуляцію, щоб забезпечити, що ці
-дані можна отримати лише через методи класу."""
-#Рівень доступу Private (приватний)
-from dataclasses import dataclass
+"""
+Завдання 2
+Реалізуйте клас "Кошик для покупок" з можливістю
+додавання товарів та підрахунку загальної вартості.
+Застосуйте інкапсуляцію для забезпечення правильності
+обробки даних."""
 
-@dataclass
-class User:
-    _name: str
-    __age: int
-    __email: str
+class ShoppingCart:
+    def __init__(self):
+        self.__items = {}#словник товарів
 
-    def get_name(self):
-        return self._name
+    def add_items(self, product, quanity, price):
+        if product not in self.__items:
+            self.__items[product] = {'quanity': quanity, 'price': price}
+        else:
+            self.__items[product]["quanity"] += quanity
 
-    def get_age(self):
-        return self.__age
 
-    def get_email(self):
-        return self.__email
 
-    def change_name(self, name): #set
-        self._name = name
-
-    def set_mail(self, mail):
-        self.__email = mail
-
-user1 = User("Ivan", 19, "example@email.com")
-print(user1.get_name())
-print(user1.get_age())
-print(user1.get_email())
-print(user1)
-user1.change_name('Ivane')
-print(user1)
-user1.set_mail("new_mail@mail.com")
-print(user1)
